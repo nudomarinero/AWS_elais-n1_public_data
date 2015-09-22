f55681e671788d9f477bfd417bcbe3756d3bab58
José Sabater Montes <jsm@iaa.es>
2015-09-22 12:17:20 +0100

    Improve robustness

--- download.py
+++ download.py
@@ -5,14 +5,13 @@ from multiprocessing import Pool
 from time import sleep
 import logging
 import os
-from subprocess import call, check_output
+from subprocess import call
 import boto
 from boto.exception import S3ResponseError
 
 #from boto.s3.key import Key
 
 bucket_name = "lofar-elais-n1"
-passive = True
 
 ## Get URLs
 file_surls = "retrieval/011/surls_successful.txt"
@@ -53,28 +52,35 @@ def download(url):
     try:
         key = bucket.get_key(key_name)
         if key is None:
+            local_file = "{path}/{f}".format(path="/home/ubuntu", f=name)
+            
+            if os.path.exists(local_file):
+                # remove stale data
+                command = "rm -rf {lf}".format(lf=local_file)
+                logging.info("Remove old version of {}".format(name))
+                logging.debug(command)
+                call(command, shell=True)
+            
             # Download the data
-            if passive:
-                command = "srmcp -server_mode=passive {srm} file:///{path}/{f}".format(srm=url, path="/home/ubuntu", f=name)
-            else:
-                command = "srmcp {srm} -globus_tcp_port_range=20000:25000 file:////{path}/{f}".format(srm=url, path="/home/ubuntu", f=name)
-                "-array_of_client_networks="
-            logging.info("Downloading {}".format(name))
-            logging.debug(command)
-            output = check_output(command, shell=True)
-            logging.debug(output)
-            
-            # Upload to the bucket
-            logging.info("Uploading {} to S3".format(name))
-            k = bucket.new_key(key_name)
-            k.set_contents_from_filename("{path}/{f}".format(srm=url, path="/home/ubuntu", f=name))
-            logging.info("Upload of {} to S3 finished".format(name))
-            
-            # remove temporary data
-            command = "rm -rf {path}/{f}".format(path="/home/ubuntu", f=name)
+            command = "srmcp -server_mode=passive {srm} file:///{lf}".format(srm=url, lf=local_file)
             logging.info("Downloading {}".format(name))
             logging.debug(command)
             call(command, shell=True)
+            
+            if os.path.exists(local_file):
+                # Upload to the bucket
+                logging.info("Uploading {} to S3".format(name))
+                k = bucket.new_key(key_name)
+                k.set_contents_from_filename(local_file)
+                logging.info("Upload of {} to S3 finished".format(name))
+                
+                # remove temporary data
+                command = "rm -rf {lf}".format(lf=local_file)
+                logging.info("Remove temporary version of {}".format(name))
+                logging.debug(command)
+                call(command, shell=True)
+            else:
+                logging.error("{} not found locally".format(name))
         else:
             logging.warn("{} is already in S3".format(name))
     except S3ResponseError:
