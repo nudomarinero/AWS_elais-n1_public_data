f9db9ff3e28eddcbeada449eefdbea213a202bad
José Sabater Montes <jsm@iaa.es>
2015-09-24 12:37:15 +0100

    Take into account if the archive is present locally

--- check_S3.py
+++ check_S3.py
@@ -29,6 +29,21 @@ if __name__ == "__main__":
     file_surls = "retrieval/011/surls_successful.txt"
     surls = [url.strip() for url in open(file_surls, "rb")]
     surls_no = check_no(surls)
+    
+    # Order
+    sn_local = []
+    sn_else = []
     for url in surls_no:
+        path, name = os.path.split(url)
+        l, sap, sb, suffix = name.split("_")
+        local_file = "{path}/{f}".format(path="/home/ubuntu", f=name)
+        if os.path.exists(local_file):
+            sn_local.append(url)
+        else:
+            sn_else.append(url)
+    surls_final = sn_local.extend(sn_else)
+        
+    
+    for url in surls_final:
         print(url)
     
